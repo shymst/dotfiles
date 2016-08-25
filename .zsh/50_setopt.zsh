f2f1b0e94d653bb1cc8bc7769bc19d8523e558c7
shymst <shymst125@gmail.com>
2016-08-25 10:05:53 +0900

    zcompdumpを無効化

--- .zsh/50_setopt.zsh
+++ .zsh/50_setopt.zsh
@@ -42,3 +42,13 @@ if which pbcopy >/dev/null 2>&1 ; then
     alias -g C='| pbcopy'
 fi
 
+for d in "/share/zsh-completions" "/share/zsh/zsh-site-functions" "/share/zsh/site-functions";do
+  brew_completion=$(brew --prefix 2>/dev/null)$d
+  if [ $? -eq 0 ] && [ -d "$brew_completion" ];then
+    fpath=($brew_completion $fpath)
+  fi
+done
+autoload -Uz compinit
+compinit -D
+
+
--- .zshenv
+++ .zshenv
@@ -1,8 +1,8 @@
-for d in "/share/zsh-completions" "/share/zsh/zsh-site-functions" "/share/zsh/site-functions";do
-  brew_completion=$(brew --prefix 2>/dev/null)$d
-  if [ $? -eq 0 ] && [ -d "$brew_completion" ];then
-    fpath=($brew_completion $fpath)
-  fi
-done
-autoload -Uz compinit
-compinit
+
+
+
+
+
+
+
+
