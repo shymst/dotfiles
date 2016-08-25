bc6cdc090b682365d709bf8ed6d006ab33f71ea3
shymst <shymst125@gmail.com>
2016-08-25 10:24:08 +0900

    brew installを追加

--- setup.sh
+++ setup.sh
@@ -5,5 +5,28 @@ do
     [[ "$f" == ".git" ]] && continue
     [[ "$f" == ".DS_Store" ]] && continue
 
-    ln -s $HOME/dotfiles/$f $HOME/$f    
+    ln -s $HOME/dotfiles/$f $HOME/$f
 done
+
+if ! type brew > /dev/null 2>&1; then
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+if ! type git > /dev/null 2>&1; then
+    brew install git
+fi
+
+if ! type vim > /dev/null 2>&1; then
+    brew install vim
+fi
+
+if ! type git-flow > /dev/null 2>&1; then
+    brew install git-flow
+fi
+
+if ! type hub > /dev/null 2>&1; then
+    brew install hub
+fi
+
+bundle install
+
