f9ca13ea1bd0b3896ea16872c7328f03ef82a4a5
Shunya Yamashita <shymst@shymst.local>
2016-09-29 22:18:29 +0900

    init.shを編集

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -1,45 +1,5 @@
 #!/bin/bash
 
-DOTPATH=~/.dotfiles
-
-# git が使えるなら git
-if has "git"; then
-    git clone --recursive "$GITHUB_URL" "$DOTPATH"
-
-# 使えない場合は curl か wget を使用する
-elif has "curl" || has "wget"; then
-    tarball="https://github.com/shymst/dotfiles/archive/master.tar.gz"
-
-    # どっちかでダウンロードして，tar に流す
-    if has "curl"; then
-        curl -L "$tarball"
-
-    elif has "wget"; then
-        wget -O - "$tarball"
-
-    fi | tar xv -
-
-    # 解凍したら，DOTPATH に置く
-    mv -f dotfiles-master "$DOTPATH"
-
-else
-    die "curl or wget required"
-fi
-
-cd ~/.dotfiles
-if [ $? -ne 0 ]; then
-    die "not found: $DOTPATH"
-fi
-
-# リンクを実行する
-for f in .??*
-do
-    [[ "$f" == ".git" ]] && continue
-    [[ "$f" == ".DS_Store" ]] && continue
-
-    ln -s $HOME/dotfiles/$f $HOME/$f
-done
-
 
 if ! type brew > /dev/null 2>&1; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -67,6 +27,7 @@ brew cask install virtualbox
 brew cask install atom
 brew cask install google-chrome
 
+
 mas install 539883307  # LINE
 mas install 921923693  # LibreOffice Vanilla
 mas install 1037994372 # DrCleanerPro
@@ -87,3 +48,76 @@ mas install 803453959  # Slack
 mas install 1007457278 # Realm Browser
 mas install 1000397973 # Wallcat
 mas install 568494494  # Pocket
+
+
+# Assign zsh as a login shell
+if ! contains "${SHELL:-}" "zsh"; then
+    zsh_path="$(which zsh)"
+
+    # Check /etc/shells
+    if ! grep -xq "${zsh_path:=/bin/zsh}" /etc/shells; then
+        log_fail "oh, you should append '$zsh_path' to /etc/shells"
+        exit 1
+    fi
+
+    if [ -x "$zsh_path" ]; then
+        # Changing for a general user
+        if chsh -s "$zsh_path" "${USER:-root}"; then
+            log_pass "Change shell to $zsh_path for ${USER:-root} successfully"
+        else
+            log_fail "cannot set '$path' as \$SHELL"
+            log_fail "Is '$path' described in /etc/shells?"
+            log_fail "you should run 'chsh -l' now"
+            exit 1
+        fi
+
+        # For root user
+        if [ ${EUID:-${UID}} = 0 ]; then
+            if chsh -s "$zsh_path" && :; then
+                log_pass "[root] change shell to $zsh_path successfully"
+            fi
+        fi
+    else
+        log_fail "$zsh_path: invalid path"
+        exit 1
+    fi
+fi
+
+
+# Install zplug
+if [[ ! -f ~/.zplug/init.zsh ]]; then
+    if (( ! $+commands[git] )); then
+        echo "git: command not found" >&2
+        exit 1
+    fi
+
+    git clone \
+        https://github.com/zplug/zplug \
+        ~/.zplug
+
+    # failed
+    if (( $status != 0 )); then
+        echo "zplug: fails to installation of zplug" >&2
+    fi
+fi
+
+if [[ -f ~/.zplug/init.zsh ]]; then
+    echo "zplug: not found" >&2
+    exit 1
+fi
+# load zplug
+source ~/.zplug/init.zsh
+
+if [[ -f $DOTPATH/.zsh/zplug.zsh ]]; then
+    export ZPLUG_LOADFILE="$DOTPATH/.zsh/zplug.zsh"
+fi
+
+if ! zplug check --verbose; then
+    printf "Install? [y/N]: "
+    if read -q; then
+        echo; zplug install
+    else
+        echo
+    fi
+fi
+zplug load --verbose
