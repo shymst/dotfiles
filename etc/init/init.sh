5a27a7669a4d1f1f0d134db567faba05ef21f4b3
Shunya Yamashita <shymst125@gmail.com>
2017-08-06 02:17:52 +0900

    zplugのインストール方法を修正

--- etc/init/init.sh
+++ etc/init/init.sh
@@ -12,7 +12,7 @@ fi
 
 run_brew
 
-[ ! -d ${HOME}/.zplug ] && curl -sL zplug.sh/installer | zsh
+[ ! -d ${HOME}/.zplug ] && curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 [ ! -d ${HOME}/.tmux/plugins/tpm ] && git clone https://github.com/tmux-plugins/tpm ${HOME}/.tmux/plugins/tpm
 
 [ ${SHELL} != "/bin/zsh"  ] && chsh -s /bin/zsh
